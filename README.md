bd13a8217030e3cd77bb3185fc954f6b0e0a3296
Sridhar Sarnobat <ss401533@gmail.com>
2020-01-08 09:43:14 -0800

    Minor changes (untested)

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # iphone_swift_hellworld
 
-### swift_build_app.sh
-```
-cd "/Users/srsarnob/Library/Autosave Information/HelloWorld/"
-xcodebuild -list -project "HelloWorld.xcodeproj"
-xcodebuild -scheme HelloWorld build
-```
--- swift_build_app.sh
+++ swift_build_app.sh
@@ -1,7 +1,1 @@
-# You might need a device to be connected to the machine so 
-# that the certificate is valid...ughhh!
-
-cd "/Users/srsarnob/Library/Autosave Information/HelloWorld/"
-xcodebuild -list -project "HelloWorld.xcodeproj"
-xcodebuild -scheme -allowProvisioningUpdates HelloWorld build
-
+run.sh
