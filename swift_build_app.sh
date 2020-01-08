cc9225f0461db12329d7c07c84cfcc7ccfbef32a
Sridhar Sarnobat <ss401533@gmail.com>
2020-01-08 09:41:55 -0800

    Minor changes (untested)

--- swift_build_app.sh
+++ swift_build_app.sh
@@ -1,1 +1,7 @@
-/Users/srsarnob/bin/swift_build_app.sh
+# You might need a device to be connected to the machine so 
+# that the certificate is valid...ughhh!
+
+cd "/Users/srsarnob/Library/Autosave Information/HelloWorld/"
+xcodebuild -list -project "HelloWorld.xcodeproj"
+xcodebuild -scheme -allowProvisioningUpdates HelloWorld build
+
